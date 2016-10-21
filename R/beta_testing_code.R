4c74c83a67304e2147a26d8706737cb5ebd8a828
askilton1 <antonio.skilton@gmail.com>
2016-10-21 10:53:07 -0700

    added files for beta testing

---
+++ R/beta_testing_code.R
@@ -0,0 +1,8 @@
+# library(tidyverse, quietly = T);library(reshape2,quietly=T)
+#
+# df <- read_csv("Disability.csv")
+# names(df) <- gsub(" ","_",names(df))
+#
+# roundto=2
+# na.rm=FALSE
+# plotText=TRUE
