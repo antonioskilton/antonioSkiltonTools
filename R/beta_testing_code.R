4e08d87340bd1b2e844a59c6963486af81f80df4
askilton1 <antonio.skilton@gmail.com>
2016-12-03 00:17:01 -0800

    Added rankedCorrelations, removed other functions
    other functions are either redundant, or replaced by new function

--- R/beta_testing_code.R
+++
@@ -1,8 +0,0 @@
-# library(tidyverse, quietly = T);library(reshape2,quietly=T)
-#
-# df <- read_csv("Disability.csv")
-# names(df) <- gsub(" ","_",names(df))
-#
-# roundto=2
-# na.rm=FALSE
-# plotText=TRUE
--- R/returnMatchedTerms.R
+++
@@ -1,6 +0,0 @@
-returnMatchedTerms <- function(terms, x){
-  index <- c()
-  sapply(x, function(y){
-    for(term in terms){index[which(terms %in% term)] <- grepl(term, y)}
-    return(terms[index])})
-}
